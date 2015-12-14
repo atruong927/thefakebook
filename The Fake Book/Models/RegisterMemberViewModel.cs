3986e880459468b7aaa0120def02c3ae7fbe4d95
atruong927 <atruong927@gmail.com>
2015-12-14 12:42:21 -0500

    Set the Date Joined to readonly so it will automatically submit the current Date and Time. Also created a RegisterMember View Model to try registering a new account and creating a new member at the same time., so the MemberID will be attached to that account. This attempt failed miserably :(

---
+++ The Fake Book/Models/RegisterMemberViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace The_Fake_Book.Models
+{
+    public class RegisterMemberViewModel
+    {
+        public RegisterViewModel RegisterViewModel
+        {
+            get; set;
+        }
+        public Member Member
+        {
+            get; set;
+        }
+    }
+}
